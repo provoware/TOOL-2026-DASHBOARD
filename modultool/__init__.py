e6199f1af54fef8da95e36b1e35131ee14ec7016
karsten tschiersch <provoware.one@gmail.com>
2025-07-21 09:28:23 +0200

    T27 init cleanup

--- modultool/__init__.py
+++ modultool/__init__.py
@@ -2,11 +2,14 @@ from .logger import logger
 from .selfcheck import run_selfcheck
 from .help_engine import register_help
 from .error_dialog import create_error_dialog
+from .event_bus import event_bus
 
 run_selfcheck()
 
-__all__ = ["logger", "run_selfcheck", "register_help", "create_error_dialog"]
-
-run_selfcheck()
-
-__all__ = ["logger", "run_selfcheck", "register_help"]
+__all__ = [
+    "logger",
+    "run_selfcheck",
+    "register_help",
+    "create_error_dialog",
+    "event_bus",
+]
