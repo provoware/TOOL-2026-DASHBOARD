8e6b03f5cbcdca9b4c4ab32b76cc5348dde81344
karsten tschiersch <provoware.one@gmail.com>
2025-07-21 09:38:51 +0200

    T28 stats sammeln

--- OFFEN.md
+++ OFFEN.md
@@ -32,7 +32,7 @@ Format: - [ ] ID (Phase) Titel :: Ziel :: Akzeptanz
 - [x] T25 (P1) Fehlerdialog UI (Benutzertext + Details)
 - [x] T26 (P1) Settings Panel (Theme, FontScale)
 - [x] T27 (P1) EventBus implementieren
-- [ ] T28 (P1) Stats sammeln (module.open)
+- [x] T28 (P1) Stats sammeln (module.open)
 - [ ] T29 (P1) Reset auf Werkzustand
 - [ ] T30 (P1) Selfcheck periodisch
 
--- modultool/__init__.py
+++ modultool/__init__.py
@@ -3,6 +3,7 @@ from .selfcheck import run_selfcheck
 from .help_engine import register_help
 from .error_dialog import create_error_dialog
 from .event_bus import event_bus
+from .stats import stats
 
 run_selfcheck()
 
@@ -12,8 +13,9 @@ __all__ = [
     "register_help",
     "create_error_dialog",
     "event_bus",
+    "stats",
 ]
 
 run_selfcheck()
 
-__all__ = ["logger", "run_selfcheck", "register_help", "event_bus"]
+__all__ = ["logger", "run_selfcheck", "register_help", "event_bus", "stats"]
