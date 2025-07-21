3a79af9f77fefa7aa93c9a353c1fb67505dea876
karsten tschiersch <provoware.one@gmail.com>
2025-07-21 09:23:42 +0200

    T27 EventBus

--- modultool/__init__.py
+++ modultool/__init__.py
@@ -2,11 +2,18 @@ from .logger import logger
 from .selfcheck import run_selfcheck
 from .help_engine import register_help
 from .error_dialog import create_error_dialog
+from .event_bus import event_bus
 
 run_selfcheck()
 
-__all__ = ["logger", "run_selfcheck", "register_help", "create_error_dialog"]
+__all__ = [
+    "logger",
+    "run_selfcheck",
+    "register_help",
+    "create_error_dialog",
+    "event_bus",
+]
 
 run_selfcheck()
 
-__all__ = ["logger", "run_selfcheck", "register_help"]
+__all__ = ["logger", "run_selfcheck", "register_help", "event_bus"]
