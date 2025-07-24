4c36b5c217f8f9aabaf1d452ddaf52cffba1ad01
karsten tschiersch <provoware.one@gmail.com>
2025-07-24 06:51:01 +0200

    cleanup keep videobatch files

--- modultool/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Module package."""
-
-from .base_module import BaseModule
-from .genres_module import GenresModule
-
-__all__ = ["BaseModule", "GenresModule"]
--- tests/test_base_module.py
+++
@@ -1,6 +0,0 @@
-from modultool.modules import BaseModule
-
-
-def test_base_module_instantiation():
-    module = BaseModule()
-    assert module.get_name() == "base"
